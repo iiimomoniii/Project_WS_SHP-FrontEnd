589aa9ff4ee4e3f8cbc516a98473fec05416642f
Natdanai <32894861+iiimomoniii@users.noreply.github.com>
2020-12-15 10:05:18 +0700

    Update Code
    create function for show sidebar follow mediaQuery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'SHP-FrontEnd';
+
+  mobileQueryMax:  MediaQueryList ;
+
+  private _mobileQueryListener: ()=> void;
+
+  constructor(changeDeterRef : ChangeDetectorRef, media : MediaMatcher){
+    //mediaQueryList = window.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDeterRef.detectChanges();
+    this.mobileQueryMax = media.matchMedia('(max-width: 600px)');
+    this.mobileQueryMax.addListener(this._mobileQueryListener);
+    
+  }
+
+  ngOnDestroy() : void{
+    this.mobileQueryMax.removeListener(this._mobileQueryListener);
+  }
 
   //alert before active sidenav
   // onSayHi(text :String){
