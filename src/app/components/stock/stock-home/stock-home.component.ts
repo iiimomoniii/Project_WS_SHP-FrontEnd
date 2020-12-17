7c0cda2fda0a2f0b70b4cc9367f730906bc456b9
Natdanai <32894861+iiimomoniii@users.noreply.github.com>
2020-12-17 16:22:42 +0700

    Update Code
    Create Datatable

--- src/app/components/stock/stock-home/stock-home.component.ts
+++ src/app/components/stock/stock-home/stock-home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatTableDataSource } from '@angular/material/table';
+import { ResProduct } from 'src/app/models/product.model';
 @Component({
   selector: 'app-stock-home',
   templateUrl: './stock-home.component.html',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StockHomeComponent implements OnInit {
 
+  displayedColumns : string[] = ['image', 'name', 'price', 'stock', 'action'];
+  dataSource = new MatTableDataSource<ResProduct>();
+
   constructor() { }
 
   ngOnInit(): void {
+    this.feedData();
   }
+
+  feedData(){
+    const mockupProduct = [
+      new ResProduct(),
+      new ResProduct(),
+      new ResProduct()
+    ]
+    this.dataSource.data = mockupProduct; 
+  }
+
+
 
 }
---
+++ src/app/models/product.model.ts
@@ -0,0 +1,6 @@
+export class ResProduct {
+    name : string;
+    stock : number;
+    price : number;
+    image : any;
+}
