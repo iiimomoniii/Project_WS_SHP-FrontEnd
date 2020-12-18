51f6bcc14c0f18829ab5ed15fd325728a6c20be4
Natdanai <32894861+iiimomoniii@users.noreply.github.com>
2020-12-18 10:08:29 +0700

    Update Code
    add mock data

--- src/app/components/stock/stock-home/stock-home.component.ts
+++ src/app/components/stock/stock-home/stock-home.component.ts
@@ -19,11 +19,11 @@ export class StockHomeComponent implements OnInit {
 
   feedData(){
     const mockupProduct : ResProduct[] = [
-     { name : "macbook", stock : 10, price : 1000, image : "https://cf.shopee.co.th/file/441a198a2c67336315b6f1fbabdae47d" },
-     { name : "macbook", stock : 10, price : 1000, image : "https://cf.shopee.co.th/file/441a198a2c67336315b6f1fbabdae47d" },
-     { name : "macbook", stock : 10, price : 1000, image : "https://cf.shopee.co.th/file/441a198a2c67336315b6f1fbabdae47d" },
-     { name : "macbook", stock : 10, price : 1000, image : "https://cf.shopee.co.th/file/441a198a2c67336315b6f1fbabdae47d" },
-     { name : "macbook", stock : 10, price : 1000, image : "https://cf.shopee.co.th/file/441a198a2c67336315b6f1fbabdae47d" },
+     { name : "macbook", stock : 1234, price : 1000, image : "https://cf.shopee.co.th/file/441a198a2c67336315b6f1fbabdae47d" },
+     { name : "macbook", stock : 3410, price : 1000, image : "https://cf.shopee.co.th/file/441a198a2c67336315b6f1fbabdae47d" },
+     { name : "macbook", stock : 4234, price : 1000, image : "https://cf.shopee.co.th/file/441a198a2c67336315b6f1fbabdae47d" },
+     { name : "macbook", stock : 43534, price : 1000, image : "https://cf.shopee.co.th/file/441a198a2c67336315b6f1fbabdae47d" },
+     { name : "macbook", stock : 43534, price : 1000, image : "https://cf.shopee.co.th/file/441a198a2c67336315b6f1fbabdae47d" },
     ];
 
     this.dataSource.data = mockupProduct; 
